0ed545c8cd68a97b5f2c27c9d027f803270164dc
ahmedShaheer3 <122429039+ahmedShaheer3@users.noreply.github.com>
2023-09-07 19:11:21 +0500

    hello commit

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,23 @@ const app = express();
 app.use(express.json());
 
 console.log(process.env) 
+console.log('index . ts is runnig ')
 
+// Default route
+app.get("/", (req: Request, res: Response) => {
+  console.log('testing changes////')
+
+  res.send('Hello "updated" world!');
+});
+// Default route
+app.get("/123", (req: Request, res: Response) => {
+  console.log('testing changes123')
+  res.send('Hello "updated" world!');
+});
 // Default route
 app.get("/", (req: Request, res: Response) => {
   res.send('Hello "updated" world!');
 });
-
 app.use("/user", userRoutes);
 // app.use('/tweet', authenticateToken, tweetRoutes);
 // app.use('/auth', authRoutes);
